8681e912044a9be37e71a0d0c7b0b33da67538ee
Jokcer <519548295@qq.com>
2024-10-20 00:10:05 +0800

    chore: update readme

--- README.md
+++ README.md
@@ -75,17 +75,34 @@ print(
 
 ```
 ### gpu版本使用
+必须下载模型，指定模型位置！
 ``` python {linenos=table}
 # 建议使用清华源安装 https://pypi.tuna.tsinghua.edu.cn/simple
 pip install rapid-table-det-paddle (默认安装gpu版本，可以自行覆盖安装cpu版本paddlepaddle)
 ```
 ```python
-#from rapid_table_det_paddle.inference import TableDetector
-#table_det = TableDetector(
-#    obj_model_path="rapid_table_det_paddle/models/obj_det_paddle",
-#    edge_model_path="rapid_table_det_paddle/models/edge_det_paddle",
-#    cls_model_path="rapid_table_det_paddle/models/cls_det_paddle",
-#)
+from rapid_table_det_paddle.inference import TableDetector
+table_det = TableDetector(
+    obj_model_path="models/obj_det_paddle",
+    edge_model_path="models/edge_det_paddle",
+    cls_model_path="models/cls_det_paddle",
+)
+# img = img_loader(img_path)
+# file_name_with_ext = os.path.basename(img_path)
+# file_name, file_ext = os.path.splitext(file_name_with_ext)
+# out_dir = "rapid_table_det_paddle/outputs"
+# if not os.path.exists(out_dir):
+#     os.makedirs(out_dir)
+# extract_img = img.copy()
+# for i, res in enumerate(result):
+#     box = res["box"]
+#     lt, rt, rb, lb = res["lt"], res["rt"], res["rb"], res["lb"]
+#     # 带识别框和左上角方向位置
+#     img = visuallize(img, box, lt, rt, rb, lb)
+#     # 透视变换提取表格图片
+#     wrapped_img = extract_table_img(extract_img.copy(), lt, rt, rb, lb)
+#     cv2.imwrite(f"{out_dir}/{file_name}-extract-{i}.jpg", wrapped_img)
+# cv2.imwrite(f"{out_dir}/{file_name}-visualize.jpg", img)
 ```
 #### 参数说明
 mode: str 模式，onnx包默认使用onnx_tiny,可选 onnx, paddle包唯一使用paddle \
--- demo_paddle.py
+++ demo_paddle.py
@@ -1,7 +1,4 @@
-import os
-import cv2
 from rapid_table_det_paddle.inference import TableDetector
-from rapid_table_det_paddle.utils import visuallize, extract_table_img, img_loader
 
 img_path = f"tests/test_files/chip.jpg"
 
@@ -19,19 +16,19 @@ print(
     f"obj_det_elapse:{obj_det_elapse}, edge_elapse={edge_elapse}, rotate_det_elapse={rotate_det_elapse}"
 )
 # 一张图片中可能有多个表格
-img = img_loader(img_path)
-file_name_with_ext = os.path.basename(img_path)
-file_name, file_ext = os.path.splitext(file_name_with_ext)
-out_dir = "rapid_table_det_paddle/outputs"
-if not os.path.exists(out_dir):
-    os.makedirs(out_dir)
-extract_img = img.copy()
-for i, res in enumerate(result):
-    box = res["box"]
-    lt, rt, rb, lb = res["lt"], res["rt"], res["rb"], res["lb"]
-    # 带识别框和左上角方向位置
-    img = visuallize(img, box, lt, rt, rb, lb)
-    # 透视变换提取表格图片
-    wrapped_img = extract_table_img(extract_img.copy(), lt, rt, rb, lb)
-    cv2.imwrite(f"{out_dir}/{file_name}-extract-{i}.jpg", wrapped_img)
-cv2.imwrite(f"{out_dir}/{file_name}-visualize.jpg", img)
+# img = img_loader(img_path)
+# file_name_with_ext = os.path.basename(img_path)
+# file_name, file_ext = os.path.splitext(file_name_with_ext)
+# out_dir = "rapid_table_det_paddle/outputs"
+# if not os.path.exists(out_dir):
+#     os.makedirs(out_dir)
+# extract_img = img.copy()
+# for i, res in enumerate(result):
+#     box = res["box"]
+#     lt, rt, rb, lb = res["lt"], res["rt"], res["rb"], res["lb"]
+#     # 带识别框和左上角方向位置
+#     img = visuallize(img, box, lt, rt, rb, lb)
+#     # 透视变换提取表格图片
+#     wrapped_img = extract_table_img(extract_img.copy(), lt, rt, rb, lb)
+#     cv2.imwrite(f"{out_dir}/{file_name}-extract-{i}.jpg", wrapped_img)
+# cv2.imwrite(f"{out_dir}/{file_name}-visualize.jpg", img)
