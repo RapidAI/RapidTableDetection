d407bdee03155f046809e04d195e0110410327d1
Jokcer <519548295@qq.com>
2024-10-19 22:02:14 +0800

    chore: fix workflow

--- tests/test_table_det.py
+++ tests/test_table_det.py
@@ -2,14 +2,13 @@ import sys
 from pathlib import Path
 
 import pytest
-from rapid_table_det import TableDetector
+
 
 cur_dir = Path(__file__).resolve().parent
 root_dir = cur_dir.parent
 
 sys.path.append(str(root_dir))
 test_file_dir = cur_dir / "test_files"
-table_det = TableDetector()
 
 
 @pytest.mark.parametrize(
@@ -17,6 +16,9 @@ table_det = TableDetector()
     [("chip.jpg", 1), ("doc.png", 2)],
 )
 def test_input_normal(img_path, expected):
+    from rapid_table_det import TableDetector
+
+    table_det = TableDetector()
     img_path = test_file_dir / img_path
     result, elapse = table_det(img_path)
     assert len(result) == expected
--- tests/test_table_det_paddle.py
+++ tests/test_table_det_paddle.py
@@ -3,21 +3,11 @@ from pathlib import Path
 
 import pytest
 
-from rapid_table_det_paddle.inference import TableDetector
-
 cur_dir = Path(__file__).resolve().parent
 root_dir = cur_dir.parent
 
 sys.path.append(str(root_dir))
 test_file_dir = cur_dir / "test_files"
-table_det = TableDetector(
-    obj_model_path=f"{root_dir}/rapid_table_det_paddle/models/obj_det_paddle",
-    edge_model_path=f"{root_dir}/rapid_table_det_paddle/models/edge_det_paddle",
-    cls_model_path=f"{root_dir}/rapid_table_det_paddle/models/cls_det_paddle",
-    use_obj_det=True,
-    use_edge_det=True,
-    use_cls_det=True,
-)
 
 
 @pytest.mark.parametrize(
@@ -25,6 +15,16 @@ table_det = TableDetector(
     [("chip.jpg", 1), ("doc.png", 2)],
 )
 def test_input_normal(img_path, expected):
+    from rapid_table_det_paddle.inference import TableDetector
+
+    table_det = TableDetector(
+        obj_model_path=f"{root_dir}/rapid_table_det_paddle/models/obj_det_paddle",
+        edge_model_path=f"{root_dir}/rapid_table_det_paddle/models/edge_det_paddle",
+        cls_model_path=f"{root_dir}/rapid_table_det_paddle/models/cls_det_paddle",
+        use_obj_det=True,
+        use_edge_det=True,
+        use_cls_det=True,
+    )
     img_path = test_file_dir / img_path
     result, elapse = table_det(img_path)
     assert len(result) == expected
